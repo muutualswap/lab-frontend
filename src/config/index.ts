09228419a91aff60b7ba56c6c26ed9358db645a7
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-01 14:13:20 -0400

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,8 +6,8 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const CAKE_PER_BLOCK = new BigNumber(1)
+export const CAKE_PER_BLOCK = new BigNumber(2)
 export const BLOCKS_PER_YEAR = new BigNumber(10512000)
 export const BSC_BLOCK_TIME = 3
 
-export const CAKE_POOL_PID = labo.pids.pidLabo
+export const CAKE_POOL_PID = bgsp.pids.pidbgsp

298f83caaa143ca3178f7d88a4a68237a18a66b7
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-01 15:41:03 -0400

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -22,7 +22,7 @@ const farms: FarmConfig[] = [
     version: 2
   },
   {
-    pid:bgsp.pids.pidList[1],
+    pid:labo.pids.pidList[1],
     risk: 5,
     lpSymbol: 'BGSP-BNB LP',
     lpAddresses: {

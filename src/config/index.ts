715d69c1f1863647c944935dcf72c913af041ff9
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-01 16:12:19 -0400

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,4 +10,4 @@ export const CAKE_PER_BLOCK = new BigNumber(2)
 export const BLOCKS_PER_YEAR = new BigNumber(10512000)
 export const BSC_BLOCK_TIME = 3
 
-export const CAKE_POOL_PID = bgsp.pids.pidbgsp
+export const CAKE_POOL_PID = labo.pids.pidlabo

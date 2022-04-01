20ff9ebdeeb623c8473e59f77f1e7705adfa2bc0
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-01 14:19:20 -0400

    Update fetchPoolsUser.ts

--- src/state/pools/fetchPoolsUser.ts
+++ src/state/pools/fetchPoolsUser.ts
@@ -93,7 +93,7 @@ export const fetchUserPendingRewards = async (account) => {
   )
 
   // Cake / Cake pool
-  const pendingReward = await masterChefContract.methods.pendingLabo('0', account).call()
+  const pendingReward = await masterChefContract.methods.pendingBGSP('0', account).call()
 
   return { ...pendingRewards, 0: new BigNumber(pendingReward).toJSON() }
 }

09eb11d31ef088d05315dcc82424ddd51e7b56d8
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-05 12:04:29 -0400

    Update fetchFarmUser.ts

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
 import erc20ABI from 'config/abi/erc20.json'
 import masterchefABI from 'config/abi/masterchef.json'
-import labo from 'config/constants/labo'
+/* import labo from 'config/constants/labo' */
 import multicall from 'utils/multicall'
 import farmsConfig from 'config/constants/farms'
 import { getMasterChefAddress } from 'utils/addressHelpers'

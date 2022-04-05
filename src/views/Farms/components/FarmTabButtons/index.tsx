3f4546b7e6b99b36e0f8c02fc229c01c622595cc
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-04 21:45:57 -0400

    Update index.tsx

--- src/views/Farms/components/FarmTabButtons/index.tsx
+++ src/views/Farms/components/FarmTabButtons/index.tsx
@@ -27,7 +27,7 @@ const FarmTabButtons = ({ stakedOnly, setStakedOnly, tokenMode }) => {
       </ActionsWrapper>
       { !tokenMode ?
         <Blablabla>
-          Follow <a target="_blanK" rel="noreferrer" href="https://docs.thelab.finance/guides/add-liquidity-labo-wbnb-lp-labo-busd-lp"><GuideLink>these steps</GuideLink></a> to add liquidity and farm with LP tokens.
+          Follow <a target="_blanK" rel="noreferrer" href="https://docs.bigswap.finance/guides/"><GuideLink>these steps</GuideLink></a> to add liquidity and farm with LP tokens.
         </Blablabla>
        : 
        "" 

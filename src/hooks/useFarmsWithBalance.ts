4309ce1094f501c4a3f4b72a59eb3eec6f7529d9
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-05 00:39:30 -0400

    Update useFarmsWithBalance.ts

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -22,7 +22,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: labo.strings.pendingX,
+        name: 'pendingBGSP',
         params: [farm.pid, account],
       }))
 

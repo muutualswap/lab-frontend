eb8db1f0cab19ca40a78e08d84ded88e75c36452
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-04 21:25:24 -0400

    Update types.ts

--- src/state/types.ts
+++ src/state/types.ts
@@ -8,7 +8,7 @@ export interface Farm extends FarmConfig {
   tokenPriceVsQuote?: BigNumber
   poolWeight?: number
   depositFeeBP?: number
-  vikingPerBlock?: number
+  BGSPPerBlock?: number
     userData?: {
     allowance: BigNumber
     tokenBalance: BigNumber

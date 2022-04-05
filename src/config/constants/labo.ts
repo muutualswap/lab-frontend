baac4f10f84bc7bf6b3142fbf45c2a4d3442caf4
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-05 10:45:20 -0400

    Update labo.ts

--- src/config/constants/labo.ts
+++ src/config/constants/labo.ts
@@ -59,4 +59,82 @@ const showFarmInfoModal = true;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
  
+// EDIT ABOVE NOT HERE
+
+const labo = {
+    addr: {
+        laboBusdAddr,
+        laboBnbAddr,
+        bgspBnbAddr,
+        laboBusdAddrV2,
+        laboBnbAddrV2,
+        bgspBnbAddrV2,
+        bgspAddr,
+        bgsp,
+        laboAddr,
+        masterChef,
+    },
+    pids: {
+        pidLaboBusd, 
+        pidLaboBnb,
+        pidLabo,
+        pidBnbBusd,
+        pidList: [
+            pidLaboBusd, // LABO-BUSD LP [0]
+            pidLaboBnb, // LABO-BNB LP [1]
+            pidBnbBusd, // BNB-BUSD LP [2]
+            1, // USDT-BUSD LP [3]
+            2, // BTCB-BNB LP [4]
+            3, // ETH-BNB LP [5]
+            4, // DAI-BUSD LP [6]
+            5, // USDC-BUSD LP [7]
+            6, // DOT-BNB LP [8]
+            7, // CAKE-BUSD LP [9]
+            8, // CAKE-BNB LP [10]
+            pidLabo, // LABO [0]
+            10, // BUSD [12]
+            11, // WBNB [13]
+            12, // EGG [14]
+            18, // BTCB [15]
+            14, // ETH [16]
+            15, // DAI [17]
+            16, // USDC [18]
+            17, // DOT [19]
+            19, // BSCX [20]
+            13, // AUTO [21]
+            22, // LABO-GYA [22]
+            23, // LABO-DSL [23]
+            24, // LABO-BUSD LP V2 [24]
+            25, // LABO-BNB LP V2 [1]
+            26, // BUSD-BNB LP V2 [26]
+            27, // USDT-BUSD LP V2 [27]
+            28, // BTCB-BNB LP V2 [28] 
+            29, // ETH-BNB LP V2 [29]
+            30, // DAI-BUSD LP V2 [30] 
+            31, // USDC-BUSD LP V2 [31]
+            31, // DOT-BNB LP V2 [32]
+            32, // CAKE-BNB LP V2 [33]
+            33, // ADA-WBNB LP V2 [34]
+
+
+        ],
+    },
+    strings: {
+        pendingBGSP
+    },
+    fetch: {
+        fetchAutomatic,
+        fetchPriceCustom
+    },
+    queryPosition: {
+        busd: busdPosition,
+        token: tokenPosition
+    },
+    isLocked: {
+        unlockWalletButton
+    },
+    isFullyConfigured,
+    showFarmInfoModal
+}
+
 export default labo;
